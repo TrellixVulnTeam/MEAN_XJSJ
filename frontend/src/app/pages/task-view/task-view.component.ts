aaa3d9a4847b9673b4389ee1bd852f34bc8fe4b7
Mrutyunjaya <me.mrutyunjaya@gmail.com>
2022-05-08 16:31:28 +0530

    dynamic lists added to UI

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -8,13 +8,17 @@ import { TaskService } from 'src/app/task.service';
   styleUrls: ['./task-view.component.scss']
 })
 export class TaskViewComponent implements OnInit {
-
+  lists:any;
   constructor(private taskService: TaskService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params:Params)=>{
-   console.log(params);
-   
+    console.log(params);
+    
+    })
+    this.taskService.getLists().subscribe((lists:any)=>{
+      // console.log(lists);
+      this.lists = lists;
     })
   }
  
